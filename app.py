3caffb3784b42ec06fee67c8db60511900980d6c
MuhammadUsman15 <80236188+MuhammadUsman15@users.noreply.github.com>
2023-09-22 14:51:59 +0500

    Update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template, jsonify
-from transformers import pipeline
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 
 app = Flask(__name__)
+
+
+model_name = "facebook/bart-large-cnn"  
 
 @app.route('/')
 def index():
@@ -13,7 +16,12 @@ def summarize():
     max_length = int(request.form['max_length'])  # Retrieve max_length from the form
     min_length = int(request.form['min_length'])  # Retrieve min_length from the form
 
-    summarizer = pipeline("summarization")
+    # Load the selected model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Create a summarization pipeline with the selected model and tokenizer
+    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
     summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
 
     return jsonify({"summary": summary[0]['summary_text']})
