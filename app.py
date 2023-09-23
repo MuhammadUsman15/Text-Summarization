fffa7849dea8d973b42d1291aa53c2cf08674735
MuhammadUsman15 <80236188+MuhammadUsman15@users.noreply.github.com>
2023-09-23 20:14:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, render_template, jsonify
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 
 app = Flask(__name__)
 
-model_name = "facebook/bart-base-cnn"  # Use a smaller model
+model_name = "facebook/bart-base-cnn"  
 
 # Load the model and tokenizer during application initialization
-model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 @app.route('/')
 def index():
@@ -19,11 +19,14 @@ def summarize():
     max_length = int(request.form['max_length'])
     min_length = int(request.form['min_length'])
 
-    # Create a summarization pipeline with the loaded model and tokenizer
-    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer)
-    summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
+    # Tokenize the input text
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
 
-    return jsonify({"summary": summary[0]['summary_text']})
+    # Generate the summary
+    summary_ids = model.generate(inputs.input_ids, max_length=max_length, min_length=min_length, num_beams=4, length_penalty=2.0, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return jsonify({"summary": summary})
 
 if __name__ == '__main__':
     app.run(debug=True)
